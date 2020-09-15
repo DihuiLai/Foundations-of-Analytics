2ac2cd44087316ae4e19850aa8ce53b3907ef818
Dihui Lai <lai.dihui@gmail.com>
2020-09-15 15:42:37 -0500

    begin update for class 2020

--- launchnotebook.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd demo/JupyterNotebook
-source analyticvenv/bin/activate
-jupyter notebook
-
--- launchvirtualenv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Create virtualenvrionment python3
-#virtualenv -p python3 analyticvenv
-cd demo/JupyterNotebook
-source analyticvenv/bin/activate
-
